823c159911c105700c532d22cb7e0ea21d477e3f
princepandey123 <74774500+princepandey123@users.noreply.github.com>
2022-07-11 15:21:15 +0530

    Update length_of_last_word.cpp

--- length of last word/length_of_last_word.cpp
+++ length of last word/length_of_last_word.cpp
@@ -1,5 +1,9 @@
 /// https://leetcode.com/problems/length-of-last-word/
 
+//Approach 1:-> Reverse the string and we have to finds the length of first word
+//Approach 2 :-> start implementing from the end of string 
+//Approach 3 :-> start iterating from the first 
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
@@ -17,6 +21,7 @@ public:
                 if(i==n){   /// end of string
                     return count;
                 }else{
+                    //WE are not reaching to end of string and there are still some character present
                     count = 0;
                 }
                 
